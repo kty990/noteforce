f3b9209eba073f739574b82b62049f38d13fc5eb
kty990 <kutcher.ty@gmail.com>
2024-04-22 22:57:04 -0600

    Needs actual add notepad functionality

--- dist/js/home.js
+++ dist/js/home.js
@@ -5,6 +5,15 @@ const insertmenu = document.getElementById("insert-menu");
 const insert_options = document.getElementById("insert-options");
 
 
+const new_notepad = document.getElementById("new-notepad");
+const notepads = document.getElementById("contents");
+
+
+function newNotepad(name) {
+    return `<p class='notepad-button'>${name}</p>`
+}
+
+
 filemenu.addEventListener("click", () => {
     insert_options.style.visibility = 'hidden';
     file_options.style.visibility = (file_options.style.visibility == 'hidden') ? 'visible' : 'hidden';
@@ -13,4 +22,8 @@ filemenu.addEventListener("click", () => {
 insertmenu.addEventListener("click", () => {
     file_options.style.visibility = 'hidden';
     insert_options.style.visibility = (insert_options.style.visibility == 'hidden') ? 'visible' : 'hidden';
+})
+
+new_notepad.addEventListener("click", () => {
+    // Display a popup window (as a div, not an actual new window) to get the name of the notepad.
 })
