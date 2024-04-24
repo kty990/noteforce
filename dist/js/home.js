b2c4509fcf9899526d514f8d93d12e58dd14a06b
kty990 <kutcher.ty@gmail.com>
2024-04-23 21:42:30 -0600

    Added test code, needs to be polished for popup

--- dist/js/home.js
+++ dist/js/home.js
@@ -24,6 +24,42 @@ insertmenu.addEventListener("click", () => {
     insert_options.style.visibility = (insert_options.style.visibility == 'hidden') ? 'visible' : 'hidden';
 })
 
-new_notepad.addEventListener("click", () => {
+new_notepad.addEventListener("click", async () => {
     // Display a popup window (as a div, not an actual new window) to get the name of the notepad.
+    let window = await newPopupWindow();
+    window = adjustPopup(window, 1);
+    document.body.appendChild(window);
 })
+
+
+
+
+
+
+/** UNTESTED CODE */
+
+async function newPopupWindow(width = 300, height = 200) {
+    return new Promise((resolve) => {
+        let w = document.createElement("div");
+        w.classList.add("popup");
+        w.style.width = `${width}px`;
+        w.style.height = `${height}px`;
+        let content = document.createElement("div");
+        content.id = "content";
+        w.appendChild(content);
+        resolve(w);
+    })
+}
+
+function adjustPopup(w, mode = 1) {
+    switch (mode) {
+        case 1:
+            // New notepad
+            let name = document.createElement("input");
+            name.style.backgroundColor = "var(--interaction)";
+            name.style.width = "70%";
+            name.style.height = "30px";
+            w.appendChild(name);
+            return w;
+    }
+}
